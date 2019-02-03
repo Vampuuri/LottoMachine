ed064bf2a9d1f96cec81434d24fe9ffd912c7eef
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-02-03 16:00:34 +0200

    IterationListener: onReceive() changes button colors
    Button colors are changed to yellow, if they were drawn in the lotto.
    When user decides their numbers, the color will change to green.

--- app/src/main/java/fi/esupponen/lottomachine/MainActivity.java
+++ app/src/main/java/fi/esupponen/lottomachine/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +15,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,15 +31,28 @@ public class MainActivity extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             Debug.print("IterationListener", "onReceive", "broadcast reveiced", 2);
 
+            for (Button b : numberButtons) {
+                if (chosenNumbers.contains(new Integer(b.getText().toString()))) {
+                    b.getBackground().setColorFilter(Color.GREEN, PorterDuff.Mode.MULTIPLY);
+                } else {
+                    b.getBackground().clearColorFilter();
+                }
+            }
+
             ArrayList<Integer> lottoNumbers = intent.getExtras().getIntegerArrayList("lottoNumbers");
             int weeks = intent.getExtras().getInt("passedWeeks");
 
             TextView tv = (TextView) findViewById(R.id.text);
-            tv.setText("Chosen numbers:\n" + chosenNumbers + "\n" + weeks + " weeks passed.");
+            tv.setText("Chosen numbers:\n" + chosenNumbers + "\n\n" + weeks + " weeks passed.");
+
+            for (Integer i : lottoNumbers) {
+                numberButtons.get(i.intValue() - 1).getBackground().setColorFilter(Color.YELLOW, PorterDuff.Mode.MULTIPLY);
+            }
         }
     }
 
     ArrayList<Integer> chosenNumbers;
+    ArrayList<Button> numberButtons;
     boolean serviceOn;
     IterationListener iListener;
 
@@ -53,8 +70,10 @@ public class MainActivity extends AppCompatActivity {
 
             if (chosenNumbers.contains(chosen)) {
                 chosenNumbers.remove(chosen);
+                ((Button)v).getBackground().clearColorFilter();
             } else if (chosenNumbers.size() < 7) {
                 chosenNumbers.add(chosen);
+                ((Button)v).getBackground().setColorFilter(Color.GREEN, PorterDuff.Mode.MULTIPLY);
             }
 
             TextView tv = (TextView) findViewById(R.id.text);
@@ -97,6 +116,53 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createNumberButtonsList();
+    }
+
+    public void createNumberButtonsList() {
+        numberButtons = new ArrayList<Button>();
+
+        numberButtons.add((Button) findViewById(R.id.number1));
+        numberButtons.add((Button) findViewById(R.id.number2));
+        numberButtons.add((Button) findViewById(R.id.number3));
+        numberButtons.add((Button) findViewById(R.id.number4));
+        numberButtons.add((Button) findViewById(R.id.number5));
+        numberButtons.add((Button) findViewById(R.id.number6));
+        numberButtons.add((Button) findViewById(R.id.number7));
+        numberButtons.add((Button) findViewById(R.id.number8));
+        numberButtons.add((Button) findViewById(R.id.number9));
+        numberButtons.add((Button) findViewById(R.id.number10));
+        numberButtons.add((Button) findViewById(R.id.number11));
+        numberButtons.add((Button) findViewById(R.id.number12));
+        numberButtons.add((Button) findViewById(R.id.number13));
+        numberButtons.add((Button) findViewById(R.id.number14));
+        numberButtons.add((Button) findViewById(R.id.number15));
+        numberButtons.add((Button) findViewById(R.id.number16));
+        numberButtons.add((Button) findViewById(R.id.number17));
+        numberButtons.add((Button) findViewById(R.id.number18));
+        numberButtons.add((Button) findViewById(R.id.number19));
+        numberButtons.add((Button) findViewById(R.id.number20));
+        numberButtons.add((Button) findViewById(R.id.number21));
+        numberButtons.add((Button) findViewById(R.id.number22));
+        numberButtons.add((Button) findViewById(R.id.number23));
+        numberButtons.add((Button) findViewById(R.id.number24));
+        numberButtons.add((Button) findViewById(R.id.number25));
+        numberButtons.add((Button) findViewById(R.id.number26));
+        numberButtons.add((Button) findViewById(R.id.number27));
+        numberButtons.add((Button) findViewById(R.id.number28));
+        numberButtons.add((Button) findViewById(R.id.number29));
+        numberButtons.add((Button) findViewById(R.id.number30));
+        numberButtons.add((Button) findViewById(R.id.number31));
+        numberButtons.add((Button) findViewById(R.id.number32));
+        numberButtons.add((Button) findViewById(R.id.number33));
+        numberButtons.add((Button) findViewById(R.id.number34));
+        numberButtons.add((Button) findViewById(R.id.number35));
+        numberButtons.add((Button) findViewById(R.id.number36));
+        numberButtons.add((Button) findViewById(R.id.number37));
+        numberButtons.add((Button) findViewById(R.id.number38));
+        numberButtons.add((Button) findViewById(R.id.number39));
+        numberButtons.add((Button) findViewById(R.id.number40));
     }
 
 
