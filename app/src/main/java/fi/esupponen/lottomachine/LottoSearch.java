ef72d7d247a07986f41143e3a777b675af00ea17
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-02-03 13:04:51 +0200

    LottoSearch: run() sends local broadcast
    run()-method sends localbroadcast once every iteration. Broadcast
    intent includes extras for the lotto of the current iteration and how
    many weeks has passed.

--- app/src/main/java/fi/esupponen/lottomachine/LottoSearch.java
+++ app/src/main/java/fi/esupponen/lottomachine/LottoSearch.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
 
 import java.util.ArrayList;
 
@@ -23,7 +24,7 @@ public class LottoSearch extends Service implements Runnable {
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle("You won the lotto!")
                         .setContentText("Congratulations! It took " + weeks + " weeks to get a match.");
-        int NOTIFICATION_ID = 12345;
+        int NOTIFICATION_ID = 1;
 
         /**Intent targetIntent = new Intent(this, MainActivity.class);
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0, targetIntent, PendingIntent.FLAG_UPDATE_CURRENT);
@@ -66,11 +67,19 @@ public class LottoSearch extends Service implements Runnable {
         while (gameOn) {
             weeks++;
             Debug.print("LottoSearch", "run", "week: " + weeks, 1);
-            if (lottoMatch(drawLotto())) {
+            ArrayList<Integer> lotto = drawLotto();
+
+            if (lottoMatch(lotto)) {
                 Debug.print("LottoSearch", "run", "WINNER FOUND", 1);
                 displayWinNotification();
                 gameOn = false;
             }
+
+            LocalBroadcastManager manager = LocalBroadcastManager.getInstance(this);
+            Intent i = new Intent("passedIteration");
+            i.putExtra("lottoNumbers", lotto);
+            i.putExtra("passedWeeks", weeks);
+            manager.sendBroadcast(i);
 
             try {
                 Thread.sleep(100);
